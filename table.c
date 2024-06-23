e7b10057b205049620864a08aeae80c046d10b42
hiunknown10000 <apk011945@gmail.com>
2024-06-24 00:18:21 +0530

    new code

---
+++ table.c
@@ -0,0 +1,24 @@
+//TABLE OF____
+#include <stdio.h>
+
+int main()
+{
+	int num, from, end;
+	printf("TABLE OF ____\n\n");
+	printf("Enter a number for the table: ");
+	scanf("%d",& num);
+	printf("The table start from: ");
+	scanf("%d",& from);
+	printf("The table end to: ");
+	scanf("%d", & end);
+	printf("The table of %d is\n\n", num);
+	
+	
+	/*printf("")
+	printf("")*/
+	do{
+	     from= from+1;
+	     printf("%d*%d=%d\n", num, from,num*from);
+	     }while(from<end);
+  return 0;
+}
