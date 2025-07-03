8008a66c733d4028818d8f6383dc222f3f91078e
hiunknown10000 <apk011945@gmail.com>
2025-07-03 22:25:34 +0530

    Add .gitignore, VSCode settings, and update bill logic
    Added .gitignore to exclude .exe files and a VSCode settings file to enable C/C++ error squiggles. Updated electric_bill.c to avoid billing if usage is 75 units or less, improving user experience.

--- electric_bill.c
+++ electric_bill.c
@@ -8,12 +8,18 @@ int main(){
     printf("Enter current unit: ");
     scanf("%d", &currentunit);
 
+    now= currentunit-preunit;
+
+    if(now<=75){
+        printf("Do not need to pay bill");
+    }
+    else{
     printf("per unit electricity cost: ");
     scanf("%d", &cost);
 
-    now= currentunit-preunit;
     bill=now*cost;
-    printf("Your electricity bill =%d .rs",bill);
+    printf("Your electricity bill = %d .rs",bill);
+    }
 
     return 0;
 }
